f557fd288a1be63256fff42f2f03e777386046a9
ElmiraHR <elhrachyan@gmail.com>
2024-09-21 20:00:54 +0200

    CustomButton added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-
-import './App.css'
-import Form from './components/form/form'
+import './App.css';
+import Form from './components/form/form';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-
-
   return (
-    <div className='globalContainer '>
-      <Form />
+    <div className='globalContainer'>
+      <Routes>
+        <Route path="/" element={<Form />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/CustomButton/CustomButton.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import s from './CustomButton.module.css';
+
+const CustomButton = ({
+  backgroundColor,
+  border,
+  padding,
+  text = 'Submit', 
+  link = '/',
+  color,
+}) => {
+  return (
+    <Link 
+      to={link} 
+      className={s.button} 
+      style={{
+        backgroundColor: backgroundColor || 'var(--color-main)',
+        border: border || 'none',
+        padding: padding || '16px 51px',
+        color: color || 'var(--color-text-white)', 
+      }}
+    >
+      {text}
+    </Link>
+  );
+};
+
+export default CustomButton;
