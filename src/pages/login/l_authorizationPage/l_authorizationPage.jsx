201ab8d62d5b096e3c27984a5c33883acafaa16b
ElmiraHR <elhrachyan@gmail.com>
2024-09-22 22:15:06 +0200

    authorization

--- src/pages/login/l_authorizationPage/l_authorizationPage.jsx
+++ src/pages/login/l_authorizationPage/l_authorizationPage.jsx
@@ -2,11 +2,21 @@ import s from "./l_authorizationPage.module.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import AuthOptions from '../../../components/authOptions/AuthOptions';
 import LanguageSwitcher from "../../../components/languageSwitcher/LanguageSwitcher";
+import logo from "../../../assets/TailBook_ecosystem.png"
+import banner from "../../../assets/banner.png"
 
 const L_authorizationPage = () => {
   return (
     <div className={s.l_authorizationPage}>
-      <LanguageSwitcher/>
+
+      <div className={s.header}>
+        <img src={logo} alt="" />
+       <LanguageSwitcher/> 
+      </div>
+      <div className={s.banner}>
+        <img src={banner} alt="" />
+        <p>Помогать животным проще вместе с FreeVet!</p>
+      </div>
       <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID">
         <AuthOptions />
       </GoogleOAuthProvider>
